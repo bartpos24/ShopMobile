1b3001fe90344c29da1f1eeb325dd7bfa8f57db7
bartpos24 <bartek.poskart24@gmail.com>
2025-01-24 22:13:01 +0100

    Moved ktlint version and spotless to .toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,18 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.diffplug.spotless")
+    alias(libs.plugins.spotless)
 }
 spotless {
     kotlin {
         target(listOf("**/*.kt")) // Wszystkie pliki Kotlin
         targetExclude(listOf("**/generated/**/*.kt")) // Wyklucz pliki wygenerowane
-        ktlint("0.50.0") // Najnowsza wersja KtLint
+        ktlint(libs.versions.ktlin.get()) // Najnowsza wersja KtLint
         trimTrailingWhitespace() // Usuwanie spacji na końcu linii
         endWithNewline() // Wymuszanie nowej linii na końcu plików
     }
     kotlinGradle {
         target(listOf("**/*.gradle.kts")) // Pliki Gradle w Kotlin DSL
-        ktlint("0.50.0")
+        ktlint(libs.versions.ktlin.get())
     }
 }
